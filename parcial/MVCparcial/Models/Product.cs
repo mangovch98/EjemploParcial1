4617bbe7ee948271b67671ea42d5a53eaedbf9ea
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-04-03 18:34:54 -0700

    commit con modelo MVC

---
+++ parcial/MVCparcial/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCparcial.Models
+{
+    public enum UniType
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Product
+    {
+        [Key]
+       public int ProductID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+        
+}
+}
